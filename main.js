566832d7d5e58306e8b9572236b1f857c93a8c84
Tejasprogrammer <78012113+Tejasprogrammer@users.noreply.github.com>
2022-05-24 13:45:27 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -7,14 +7,35 @@ function setup()
 {
    canvas=createCanvas(300,300);
    canvas.center()
+   video=createCapture(VIDEO);
+   video.size(300,300);
+   video.hide();
+
+   poseNet = ml5.poseNet(video, modelLoaded);
+   poseNet.on('pose', gotposes)
+}
+
+function modelLoaded()
+{
+console.log('PoseNet is Initialized');
 }
 
 function draw()
 {
-
+image(video,0,0,300,300);
 }
 
 function take_snapshot()
 {
-    Save('my_filter_image.png');
+    save('my_Filter_image.png');
+}
+
+function gotposes(results)
+{
+    if(results.length>0)
+    {
+        console.log(results);
+        console.log("Nose X =" +results[0].pose.nose.x);
+        console.log("Nose Y =" +results[0].pose.nose.y);
+    }
 }
